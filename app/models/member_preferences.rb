23977df5911e4277ef3836c7d2b7cb720e2b8d90
Kevin Miller <kevm2@yahoo.com>
2015-08-25 13:09:40 -0700

    Error corrections

--- app/models/member_preferences.rb
+++ app/models/member_preferences.rb
@@ -1,4 +1,4 @@
 class MemberPreference
   include ActiveModel::Model
-  attr_accessor :minage, :maxage, :gender: :smoker, :drinker, :hobbies
+  attr_accessor :minage, :maxage, :gender, :smoker, :drinker, :hobbies
 end
