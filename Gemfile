da9039ec80d880a23485b52a1ad117ac716d2675
Kevin Miller <kevm2@yahoo.com>
2015-08-25 09:50:58 -0700

    devise integration

--- Gemfile
+++ Gemfile
@@ -16,6 +16,8 @@ gem 'coffee-rails', '~> 4.1.0'
 gem 'rails-boilerplate'
 gem 'bootstrap-sass'
 gem 'paperclip'
+gem 'devise'
+
 # See https://github.com/rails/execjs#readme for more supported runtimes
 # gem 'therubyracer', platforms: :ruby
 
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150825144020) do
+ActiveRecord::Schema.define(version: 20150825165005) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -23,6 +23,15 @@ ActiveRecord::Schema.define(version: 20150825144020) do
   end
 
   add_index "contacted_members", ["member"], name: "index_contacted_members_on_member", using: :btree
+
+  create_table "hobbies", force: :cascade do |t|
+    t.string   "hobby_name"
+    t.integer  "member_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  add_index "hobbies", ["member_id"], name: "index_hobbies_on_member_id", using: :btree
 
   create_table "member_infos", force: :cascade do |t|
     t.integer  "age"
@@ -59,6 +68,24 @@ ActiveRecord::Schema.define(version: 20150825144020) do
   end
 
   add_index "messages", ["member_id"], name: "index_messages_on_member_id", using: :btree
+
+  create_table "users", force: :cascade do |t|
+    t.string   "email",                  default: "", null: false
+    t.string   "encrypted_password",     default: "", null: false
+    t.string   "reset_password_token"
+    t.datetime "reset_password_sent_at"
+    t.datetime "remember_created_at"
+    t.integer  "sign_in_count",          default: 0,  null: false
+    t.datetime "current_sign_in_at"
+    t.datetime "last_sign_in_at"
+    t.inet     "current_sign_in_ip"
+    t.inet     "last_sign_in_ip"
+    t.datetime "created_at",                          null: false
+    t.datetime "updated_at",                          null: false
+  end
+
+  add_index "users", ["email"], name: "index_users_on_email", unique: true, using: :btree
+  add_index "users", ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true, using: :btree
 
   add_foreign_key "member_infos", "members"
 end
